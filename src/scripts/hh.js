70f0f3a5803204a99b8792365fb7d4727208741f
Mihei03 <70269913+Mihei03@users.noreply.github.com>
2024-03-10 23:42:04 +0700

    Доработка

--- src/scripts/hh.js
+++ src/scripts/hh.js
@@ -3,20 +3,28 @@ function createVacancyWidget(vacancyData) {
     const vacancyWidgetElement = document.getElementById('vacancy-widget');
     const vacancyElement = document.createElement('div');
     vacancyElement.classList.add('vacancy-card');
-  
+
+    const importantData = {
+        name: vacancyData.name,
+        salary: `${vacancyData.salary.from} ${vacancyData.salary.currency}`,
+        experience: vacancyData.experience.name,
+        employment: vacancyData.employment.name,
+        alternate_url: vacancyData.alternate_url,
+    };
+
     vacancyElement.innerHTML = `
-        <h2>${vacancyData.name}</h2>
-        <p>Зарплата: ${vacancyData.salary.from} ${vacancyData.salary.currency}</p>
-        <p>Требуемый опыт работы: ${vacancyData.experience.name}</p>
-        <p>Тип занятости: ${vacancyData.employment.name}</p>
-        <a href="${vacancyData.alternate_url}" target="_blank">Подробнее</a>
+        <h2>${importantData.name}</h2>
+        <p>Зарплата: ${importantData.salary}</p>
+        <p>Требуемый опыт работы: ${importantData.experience}</p>
+        <p>Тип занятости: ${importantData.employment}</p>
+        <a href="${importantData.alternate_url}" target="_blank">Подробнее</a>
     `;
-  
+
     vacancyWidgetElement.appendChild(vacancyElement);
-  }
-  
-  // Запрос данных о вакансии с помощью API hh.ru
-  axios.get('https://api.hh.ru/vacancies/7760476')
+}
+
+// Запрос данных о вакансии с помощью API hh.ru
+axios.get('https://api.hh.ru/vacancies/7760476')
     .then((response) => {
         const vacancyData = response.data;
         createVacancyWidget(vacancyData);
@@ -24,4 +32,3 @@ function createVacancyWidget(vacancyData) {
     .catch((error) => {
         console.error('Ошибка при загрузке данных о вакансии:', error);
     });
-  
